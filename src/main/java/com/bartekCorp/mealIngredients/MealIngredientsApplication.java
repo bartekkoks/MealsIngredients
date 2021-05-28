0c58b31c6cb22b623406f60e0281d79fd8de4acf
bartekkoks <bartek13madry@gmail.com>
2021-05-29 00:05:44 +0200

    Add files via upload

---
+++ src/main/java/com/bartekCorp/mealIngredients/MealIngredientsApplication.java
@@ -0,0 +1,13 @@
+package com.bartekCorp.mealIngredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealIngredientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealIngredientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartekCorp/mealIngredients/MealIngredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartekCorp.mealIngredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealIngredientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
